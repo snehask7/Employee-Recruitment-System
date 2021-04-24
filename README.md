8028b66d2979a0616ef784f61d21c10de5fe74ae
snehask7 <snehaskannan@gmail.com>
2021-04-24 14:27:07 +0530

    Completed Interviewer Pages

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ A React.js front end for the Employee Recruitment System
 ## To run this
 
 1. Run `npm install`
-2. Fill the .env file with the example format given (.env.example)
+2. Fill the .env file with contents in the .env.example file
 3. Run `npm start`
--- src/components/Interviewer/InterviewerNav.js
+++ src/components/Interviewer/InterviewerNav.js
@@ -1,9 +1,7 @@
 import Cookies from 'js-cookie';
 import React, { useState } from "react";
 import { AiOutlinePoweroff } from "react-icons/ai";
-import { BsPersonSquare } from "react-icons/bs";
-import { MdHome, MdMenu, MdSearch } from "react-icons/md";
-import { RiProfileLine } from "react-icons/ri";
+import { MdHome, MdMenu } from "react-icons/md";
 import { Menu, MenuItem, ProSidebar } from "react-pro-sidebar";
 import "react-pro-sidebar/dist/css/styles.css";
 import { Link, useHistory } from "react-router-dom";
@@ -43,41 +41,12 @@ const Nav = props => {
         </MenuItem>
         <hr></hr>
         <MenuItem>
-          <Link style={{ color: 'white' }} to="/ApplicantDashboard">
+          <Link style={{ color: 'white' }} to="/InterviewerDashboard">
             <MdHome
               size={28}
               style={{ marginLeft: "2px", marginRight: "1em" }}
             ></MdHome>
             Home
-          </Link>
-        </MenuItem>
-        <MenuItem>
-          <Link style={{ color: 'white' }} to="/SearchJob">
-            <MdSearch
-              size={26}
-              style={{ marginLeft: "2px", marginRight: "1em" }}
-            ></MdSearch>
-            Search for Jobs{" "}
-          </Link>
-        </MenuItem>
-        <MenuItem>
-          <Link style={{ color: 'white' }} to="/ViewApplications">
-            <RiProfileLine
-              onClick={() => collapse()}
-              size={26}
-              style={{ marginLeft: "2px", marginRight: "1em" }}
-            ></RiProfileLine>
-          View Applications
-          </Link>
-        </MenuItem>
-        <MenuItem>
-          <Link style={{ color: 'white' }} to="/ApplicantProfile/EditProfile">
-            <BsPersonSquare
-              onClick={() => collapse()}
-              size={23}
-              style={{ marginLeft: "3px", marginRight: "1em" }}
-            ></BsPersonSquare>
-          &nbsp;Profile
           </Link>
         </MenuItem>
         <hr></hr>
